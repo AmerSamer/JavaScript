80d57a19504dd92bd0266f100e0a31ca944a8c81
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-13 00:19:03 +0300

    2.2 fortune_teller

---
+++ 2.2-fortune_teller/fortune.js
@@ -0,0 +1,5 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+    console.log('You will be a ' +jobTitle + ' in ' +location+ ' and married to ' +partnerName+' with '+ numberOfChildren+'.');
+}
+
+tellFortune('programmer', 'Tel Aviv', 'Sharon', '2 children');
