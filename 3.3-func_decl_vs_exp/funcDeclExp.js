e0a27d9ad6458277eef6c1f0e795a8b824b7ab9d
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-13 09:47:49 +0300

    3.3 func del exp

--- 3.3-func_decl_vs_exp/funcDeclExp.js
+++ 3.3-func_decl_vs_exp/funcDeclExp.js
@@ -1,5 +1,19 @@
 function percentageOfWorld1(population){
     const pop = 7900000000;
-    console.log((population/pop)*100 + '%');
+    return (population/pop)*100;
 }
-percentageOfWorld1(1441000000);
+// percentageOfWorld1(1441000000);
+let china = percentageOfWorld1(1441000000);
+console.log(china + '$');  
+let spain = percentageOfWorld1(46776422);
+console.log(spain + '$');
+let italy = percentageOfWorld1(60355544);
+console.log(italy + '$');
+
+let percentageOfWorld2 = function (population) {
+    const pop = 7900000000;
+    return (population/pop)*100;
+};
+console.log(percentageOfWorld2(1441000000) + '$');
+// console.log(percentageOfWorld2(46776422));
+// console.log(percentageOfWorld2(60355544));
