c8584e6f6a9c0931e21dc6bb42a5d32f1a80bab8
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-19 20:23:10 +0300

    10.3 maps and foreach

---
+++ 10.3-massaging_data_with_map/massaging_data_with_map.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        name: "John",
+        birthday: "1-1-1995",
+        favoriteFoods: {
+            meats: ["hamburgers", "sausages"],
+            fish: ["salmon", "pike"],
+        },
+    },
+    {
+        name: "Mark",
+        birthday: "10-5-1980",
+        favoriteFoods: {
+            meats: ["hamburgers", "steak", "lamb"],
+            fish: ["tuna", "salmon", "barracuda"],
+        },
+    },
+    {
+        name: "Mary",
+        birthday: "1-10-1977",
+        favoriteFoods: {
+            meats: ["ham", "chicken"],
+            fish: ["pike"],
+        },
+    },
+    {
+        name: "Thomas",
+        birthday: "1-10-1990",
+        favoriteFoods: {
+            meats: ["bird", "rooster"],
+            fish: ["salmon"],
+        },
+    },
+    {
+        name: "Mary",
+        birthday: "1-10-1977",
+        favoriteFoods: {
+            meats: ["hamburgers", "lamb"],
+            fish: ["anchovies", "tuna"],
+        },
+    },
+];
+
+//////////////// A
+
+let allNames = data.map(function (n){
+    return n.name
+})
+
+// console.log(allNames);
+
+//////////////// B
+let before90 = data.filter(function (b){
+    return b.birthday.split('-')[2] < '1990'
+})
+
+// console.log(before90);
+
+//////////////// C
