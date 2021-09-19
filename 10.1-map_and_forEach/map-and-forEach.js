79a51283eb9470069d158afc582ebf40cad55dd1
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-19 18:38:38 +0300

    10.1 map and forEach

---
+++ 10.1-map_and_forEach/map-and-forEach.js
@@ -0,0 +1,46 @@
+const newReleases = [
+    {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+    },
+    {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+    },
+    {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+    },
+    {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+    },
+    ];
+
+    newReleases.forEach(function (b){
+        // return b.id + ' ' + b.title
+         console.log(b.id + ' ' + b.title);
+    })
+
+    // console.log(newReleases);
+    const arr = newReleases.map(function (t){
+        return t.id + ' ' + t.title
+    })
+
+    console.log(arr);
