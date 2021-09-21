20d68d9f519e1118989b1fa63bf3e88d351fb770
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-21 17:29:44 +0300

    13.2 while loop

--- 13.2-while_loop/while_loop.js
+++ 13.2-while_loop/while_loop.js
@@ -1,17 +1,4 @@
-function arrLength(){
-    let arr = ["boo", "doooo", "hoo","ro"];
-    let arrayTemp = [];
-    for(let i = 0 ; i < arr.length ; i++){
-        let x = arr[i].length;
-        arrayTemp.push(x);
-    }
-    console.log(arrayTemp);
-}
 
-
-arrLength();
-
-/////////////////////////////
 let arr = ["boo", "doooo", "hoo","ro"];
 let arrayTemp = [];
 let i = 0
@@ -20,3 +7,4 @@ while(i < arr.length){
     arrayTemp.push(x);
     i++
 }
+console.log(arrayTemp);
