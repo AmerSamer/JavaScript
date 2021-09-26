f710813913cadae8e87c28618444f992d1695aea
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-09-26 22:20:51 +0300

    18.2

---
+++ 18.2-create_web_page/index.js
@@ -0,0 +1,8 @@
+const div = document.querySelector('div')
+
+div.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea debitis aspernatur minus at iure sint vel amet dolore facilis officia!'
+
+var myImage = new Image(500, 500);
+myImage.src = 'picture.jpg';
+document.body.appendChild(myImage);
+
