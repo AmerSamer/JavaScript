1f2b5c6e2bbd0356580e6e40fcebcd092fc5651d
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-05 15:27:38 +0300

    STAR WARS

--- 34.1-star_wars/script.js
+++ 34.1-star_wars/script.js
@@ -1,6 +1,6 @@
 async function peoples() {
     let array = []
-    for (let i = 1; i <= 10 ; i++) {
+    for (let i = 1; i <= 10; i++) {
         let res = await (await fetch(`https://swapi.dev/api/people/${i}/`)).json()
         let resPlanet = await (await fetch(`https://swapi.dev/api/planets/${i}/`)).json()
 
@@ -21,30 +21,30 @@ async function peoples() {
 
     let table = document.querySelector('#mytable')
     let out = ''
-    for(let i = 0 ; i < 11 ; i++){
+    for (let i = 0; i < 11; i++) {
         out += '<tr>'
-        for(let j = 0 ; j < 6 ; j++){
-            if(j == 0 && i == 0)
-            out += '<th>name</th>'
-            if(j == 1 && i == 0)
-            out += '<th>hair</th>'
-            if(j == 2 && i == 0)
-            out += '<th>height</th>'
-            if(j == 3 && i == 0)
-            out += '<th>planet name</th>'
-            if(j == 4 && i == 0)
-            out += '<th>planet population</th>'
+        for (let j = 0; j < 6; j++) {
+            if (j == 0 && i == 0)
+                out += '<th>name</th>'
+            if (j == 1 && i == 0)
+                out += '<th>hair</th>'
+            if (j == 2 && i == 0)
+                out += '<th>height</th>'
+            if (j == 3 && i == 0)
+                out += '<th>planet name</th>'
+            if (j == 4 && i == 0)
+                out += '<th>planet population</th>'
 
-            if(j == 1 && i != 0)
-            out += '<th>' + array[i-1].name + '</th>'
-            if(j == 2 && i != 0)
-            out += '<th>' + array[i-1].hair + '</th>'
-            if(j == 3 && i != 0)
-            out += '<th>' + array[i-1].height + '</th>'
-            if(j == 4 && i != 0)
-            out += '<th>' + array[i-1].planet.name + '</th>'
-            if(j == 5 && i != 0)
-            out += '<th>' + array[i-1].planet.population + '</th>'
+            if (j == 1 && i != 0)
+                out += '<th>' + array[i - 1].name + '</th>'
+            if (j == 2 && i != 0)
+                out += '<th>' + array[i - 1].hair + '</th>'
+            if (j == 3 && i != 0)
+                out += '<th>' + array[i - 1].height + '</th>'
+            if (j == 4 && i != 0)
+                out += '<th>' + array[i - 1].planet.name + '</th>'
+            if (j == 5 && i != 0)
+                out += '<th>' + array[i - 1].planet.population + '</th>'
         }
         out += '</tr>'
     }
