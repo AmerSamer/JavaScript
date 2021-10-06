24b0cbec3233a9df0b653ce7fbad8910f7b1061a
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-06 15:38:24 +0300

    Trivia

--- trivia_api/script.js
+++ trivia_api/script.js
@@ -1,16 +1,25 @@
 async function trivia() {
     let array = []
+    let arrayDog = []
     let checker = document.querySelector('.checker')
     checker.innerHTML = ''
-    let res = await (await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`)).json()
+    let res = await (await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`)).json();
     console.log(res);
+    let dog = await (await fetch(`https://dog.ceo/api/breeds/image/random`)).json();
+    console.log(dog);
+    let dogg = document.querySelector('.dog')
+    dogg.innerHTML = ''
     for (let i = 0; i < 1; i++) {
         // let res = await (await fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)).json()
         // console.log(res);
         // let res = await (await fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)).json()
         // let resPlanet = await (await fetch(`https://swapi.dev/api/planets/${i}/`)).json()
         // console.log(res);
-
+        let objDog={
+            message: dog.message,
+            status: dog.status
+        }
+        // console.log(dog.message);
         let obj = {
             qNum: i,
             question: res.results[i].question,
@@ -19,6 +28,9 @@ async function trivia() {
             incorrect_answers: res.results[i].incorrect_answers,
             answers: (res.results[i].correct_answer + ',' + res.results[i].incorrect_answers).split(','),
         }
+        
+        arrayDog.push(objDog)
+        console.log(arrayDog[0].message);
         console.log(obj);
         array.push(obj)
 
@@ -74,15 +86,18 @@ async function trivia() {
         // console.log(trueAnswer);
         Answer1.addEventListener('click', () => {
             let checker = document.querySelector('.checker')
+            let dogg = document.querySelector('.dog')
             if (array[0].answers[first] == array[0].correct_answer) {
                 checker.style.color = "white"
                 checker.style.background= "green"
                 checker.innerHTML = 'True'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
                 console.log('succ');
             } else {
                 checker.style.color = "white"
                 checker.style.background = "red"
                 checker.innerHTML = 'False'
+                dogg.innerHTML= `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
                 console.log('fail');
             }
             // checker.innerHTML = ''
@@ -93,14 +108,19 @@ async function trivia() {
         })
         Answer2.addEventListener('click', () => {
             let checker = document.querySelector('.checker')
+            let dogg = document.querySelector('.dog')
             if (array[0].answers[second] == array[0].correct_answer) {
                 checker.style.color = "white"
                 checker.style.background= "green"
                 checker.innerHTML = 'True'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             } else {
                 checker.style.color = "white"
                 checker.style.background = "red"
                 checker.innerHTML = 'False'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             }
             setTimeout(()=>{ 
                 trivia() 
@@ -108,14 +128,19 @@ async function trivia() {
         })
         Answer3.addEventListener('click', () => {
             let checker = document.querySelector('.checker')
+            let dogg = document.querySelector('.dog')
             if (array[0].answers[third] == array[0].correct_answer) {
                 checker.style.color = "white"
                 checker.style.background= "green"
                 checker.innerHTML = 'True'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             } else {
                 checker.style.color = "white"
                 checker.style.background = "red"
                 checker.innerHTML = 'False'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             }
             setTimeout(()=>{ 
                 trivia() 
@@ -123,14 +148,19 @@ async function trivia() {
         })
         Answer4.addEventListener('click', () => {
             let checker = document.querySelector('.checker')
+            let dogg = document.querySelector('.dog')
             if (array[0].answers[fourth] == array[0].correct_answer) {
                 checker.style.color = "white"
                 checker.style.background= "green"
                 checker.innerHTML = 'True'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             } else {
                 checker.style.color = "white"
                 checker.style.background = "red"
                 checker.innerHTML = 'False'
+                dogg.innerHTML=  `<img style="width: 100px; height: 100px;" src='${arrayDog[0].message}'>`
+
             }
             setTimeout(()=>{ 
                 trivia() 
