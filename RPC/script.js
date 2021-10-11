da34c3e5171c1ec808d81df6c773c1f2d89b09ea
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-10-11 16:18:31 +0300

    RPC

--- RPC/script.js
+++ RPC/script.js
@@ -11,59 +11,59 @@ let computerScore = 0
 let tieScore = 0
 let i = 0
 let arrayComputer = ['rock', 'paper', 'scissors']
-    
+
 ////////////////////////////////////////////////////////////////////////////////////////////
-  
-    rock.addEventListener('click', outputPlayer)
-    paper.addEventListener('click', outputPlayer1)
-    scissors.addEventListener('click', outputPlayer2)
 
-    function outputPlayer() {
-        boardGame.innerHTML = 'rock'
-        return outputComputer('rock')
+rock.addEventListener('click', outputPlayer)
+paper.addEventListener('click', outputPlayer1)
+scissors.addEventListener('click', outputPlayer2)
+
+function outputPlayer() {
+    boardGame.innerHTML = 'rock'
+    return outputComputer('rock')
+}
+function outputPlayer1() {
+    boardGame.innerHTML = 'paper'
+    return outputComputer('paper')
+}
+function outputPlayer2() {
+    boardGame.innerHTML = 'scissors'
+    return outputComputer('scissors')
+}
+function outputComputer(outputPlayer) {
+    const shuffledArrayComputer = arrayComputer.sort((a, b) => 0.5 - Math.random());
+    ComputerGame.innerHTML = shuffledArrayComputer[0]
+    return score(outputPlayer, shuffledArrayComputer[0])
+}
+function score(outputPlayer, outputComputer) {
+    if (outputPlayer == 'rock' && outputComputer == 'rock' ||
+        outputPlayer == 'paper' && outputComputer == 'paper' ||
+        outputPlayer == 'scissors' && outputComputer == 'scissors') {
+        i++
+        result.innerHTML = `Tie Round ${i}`
+        tieScore++
+        total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
+    } else if (outputPlayer == 'rock' && outputComputer == 'paper' ||
+        outputPlayer == 'scissors' && outputComputer == 'rock' ||
+        outputPlayer == 'paper' && outputComputer == 'scissors') {
+        i++
+        result.innerHTML = `Computer win Round ${i}`
+        computerScore++
+        total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
     }
-    function outputPlayer1() {
-        boardGame.innerHTML = 'paper'
-        return outputComputer('paper')
+    else if (outputComputer == 'rock' && outputPlayer == 'paper' ||
+        outputComputer == 'scissors' && outputPlayer == 'rock' ||
+        outputComputer == 'paper' && outputPlayer == 'scissors') {
+        i++
+        result.innerHTML = `Player win Round ${i}`
+        playerScore++
+        total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
     }
-    function outputPlayer2() {
-        boardGame.innerHTML = 'scissors'
-        return outputComputer('scissors')
-    }
-    function outputComputer(outputPlayer) {
-        const shuffledArrayComputer = arrayComputer.sort((a, b) => 0.5 - Math.random());
-        ComputerGame.innerHTML = shuffledArrayComputer[0]
-        return score(outputPlayer, shuffledArrayComputer[0])
-    }
-    function score(outputPlayer, outputComputer) {
-        if (outputPlayer == 'rock' && outputComputer == 'rock' ||
-            outputPlayer == 'paper' && outputComputer == 'paper' ||
-            outputPlayer == 'scissors' && outputComputer == 'scissors') {
-            i++
-            result.innerHTML = `Tie Round ${i}`
-            tieScore++
-            total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
-        } else if (outputPlayer == 'rock' && outputComputer == 'paper' ||
-            outputPlayer == 'scissors' && outputComputer == 'rock' ||
-            outputPlayer == 'paper' && outputComputer == 'scissors') {
-            i++
-            result.innerHTML = `Computer win Round ${i}`
-            computerScore++
-            total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
-        }
-        else if (outputComputer == 'rock' && outputPlayer == 'paper' ||
-            outputComputer == 'scissors' && outputPlayer == 'rock' ||
-            outputComputer == 'paper' && outputPlayer == 'scissors') {
-            i++
-            result.innerHTML = `Player win Round ${i}`
-            playerScore++
-            total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
-        }
-    }
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-    total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
 
-    end.addEventListener('click', ()=>{
-        location.reload();
-    })
+total.innerHTML = `Player: ${playerScore} Tie: ${tieScore}, Computer: ${computerScore}`
+
+end.addEventListener('click', () => {
+    location.reload();
+})
